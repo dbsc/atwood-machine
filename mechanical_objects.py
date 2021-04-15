97797f8112e360ae7724cdd784ad1a1789477063
Batata <andredalbosco2020@gmail.com>
2021-04-14 21:52:30 -0300

    refactoration of string object

--- mechanical_objects.py
+++ mechanical_objects.py
@@ -1,6 +1,7 @@
 from itertools import chain
 from manim import *
 from polar import cartesian_to_polar, polar_to_cartesian
+# from atwood import AtwoodMachine
 
 
 class Mass(Circle):
@@ -22,19 +23,74 @@ class Pulley(VGroup):
 
 class AtwoodString(VGroup):
 
-    def __init__(self, pulley_radius, fixed_point, left_pulley, right_pulley, left_mass, right_mass, theta1, theta2, **kwargs):
-        def get_left_half(radius, fixed_point, pulley, mass, theta):
-            middle = Line(fixed_point.get_center() + radius * UP, pulley.get_center() + radius * UP).get_points()
-            arc = Arc(PI / 2, PI / 2 - theta.get_value(), radius=radius, arc_center=pulley.get_center()).get_points()
-            lower = Line(pulley.get_center() + polar_to_cartesian(radius, PI - theta.get_value()),
-                           mass.get_center() + polar_to_cartesian(mass.radius, PI / 2 - theta.get_value())).get_points()
-            return [p for p in chain(middle, arc, lower)]
-        def get_right_half(radius, fixed_point, pulley, mass, theta):
-            middle = Line(fixed_point.get_center() + radius * UP, pulley.get_center() + radius * UP).get_points()
-            arc = Arc(PI / 2, theta.get_value() - PI / 2, radius=radius, arc_center=pulley.get_center()).get_points()
-            lower = Line(pulley.get_center() + polar_to_cartesian(radius, theta.get_value()),
-                           mass.get_center() + polar_to_cartesian(mass.radius, PI / 2 + theta.get_value())).get_points()
-            return [p for p in chain(middle, arc, lower)]
-        self.left_half = Line().add_updater(lambda m: m.set_points(get_left_half(pulley_radius, fixed_point, left_pulley, left_mass, theta1)))
-        self.right_half = Line().add_updater(lambda m: m.set_points(get_right_half(pulley_radius, fixed_point, right_pulley, right_mass, theta2)))
-        super().__init__(self.left_half, self.right_half)
+    def __init__(self, atwood, **kwargs):
+        self.atwood = atwood
+        self.left_half = Line().add_updater(lambda m: m.set_points(self.get_left_half()))
+        self.right_half = Line().add_updater(lambda m: m.set_points(self.get_right_half()))
+        super().__init__(self.left_half, self.right_half, **kwargs)
+
+    def get_left_half(self):
+        middle = Line(self.string_center, self.left_pulley_center +
+                      UP * self.radius).get_points()
+        arc = Arc(PI / 2, PI / 2 - self.theta1, self.radius,
+                  arc_center=self.left_pulley_center).get_points()
+        lower = Line(self.left_pulley_center + polar_to_cartesian(self.radius, PI - self.theta1),
+                     self.left_mass_center + polar_to_cartesian(self.left_mass_radius, PI / 2 - self.theta1)).get_points()
+        return [p for p in chain(middle, arc, lower)]
+
+    def get_right_half(self):
+        middle = Line(self.string_center,
+                      self.right_pulley_center + UP * self.radius).get_points()
+        arc = Arc(PI / 2, self.theta2 - PI / 2, self.radius,
+                  arc_center=self.right_pulley_center).get_points()
+        lower = Line(self.right_pulley_center + polar_to_cartesian(self.radius, self.theta2),
+                     self.right_mass_center + polar_to_cartesian(self.right_mass_radius, PI / 2 + self.theta2)).get_points()
+        return [p for p in chain(middle, arc, lower)]
+
+    @property
+    def radius(self):
+        return self.atwood.pulley_radius
+
+    @property
+    def string_center(self):
+        return self.atwood.fixed_center.get_center() + UP * self.radius
+
+    @property
+    def left_pulley_center(self):
+        return self.atwood.left_pulley.get_center()
+
+    @property
+    def right_pulley_center(self):
+        return self.atwood.right_pulley.get_center()
+
+    @property
+    def left_mass_center(self):
+        return self.atwood.get_left_mass_position()
+
+    @property
+    def right_mass_center(self):
+        return self.atwood.get_right_mass_position()
+
+    @property
+    def theta1(self):
+        return self.atwood.theta1.get_value()
+
+    @property
+    def theta2(self):
+        return self.atwood.theta2.get_value()
+
+    @property
+    def l1(self):
+        return self.atwood.l1.get_value()
+
+    @property
+    def l2(self):
+        return self.atwood.l2.get_value()
+
+    @property
+    def left_mass_radius(self):
+        return self.atwood.left_mass.height / 2
+
+    @property
+    def right_mass_radius(self):
+        return self.atwood.right_mass.height / 2
