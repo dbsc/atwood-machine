a187199199d0cbc8bc7316ad517093064f419df2
Batata <andredalbosco2020@gmail.com>
2021-04-15 21:20:04 -0300

    corrected differential equation

--- diffeq.py
+++ diffeq.py
@@ -3,8 +3,8 @@ from numpy import sin, cos
 
 def atwood_diffeq_system(y, t, g, r, m1, m2, l1, l2):
     x, v, theta1, omega1, theta2, omega2 = y
-    alfa1 = (omega1**2 * r - g * sin(theta1)) / l1
-    alfa2 = (omega2**2 * r - g * sin(theta2)) / l2
+    alfa1 = (omega1**2 * r - g * sin(theta1) - 2 * (r * omega1 - v) * omega1) / l1
+    alfa2 = (omega2**2 * r - g * sin(theta2) - 2 * (v + r * omega2) * omega2) / l2
     a = (m2 * (l2 * omega2**2 + g * cos(theta2)) -
          m1 * (l1 * omega1**2 + g * cos(theta1))) / (m1 + m2)
     return [v, a, omega1, alfa1, omega2, alfa2]
