bb4c5d62a1d86ebd72a9f892ecd5090220f6e276
Batata <andredalbosco2020@gmail.com>
2021-05-27 17:40:37 -0300

    update atwood class

--- atwood.py
+++ atwood.py
@@ -1,6 +1,6 @@
 from mechanical_objects import AtwoodString, Mass, Pulley
 from diffeq import atwood_diffeq_system
-from misc import polar_to_cartesian, stretch
+from misc import polar_to_cartesian
 from scipy.integrate import odeint
 from manim import *
 
@@ -43,82 +43,124 @@ class AtwoodMachine(VGroup):
         "stroke_color": WHITE,
         "stroke_width": 0.25 * DEFAULT_STROKE_WIDTH,
     }
-    VALID_PARAMETERS = [
-        'gravity',
-        'separation',
-        'pulley_radius',
-        'initial_string_velocity',
-        'left_mass_style',
-        'right_mass_style',
-        'pulley_style',
-    ]
 
-    def __init__(self, **params):
-        super().__init__()
-        for key, value in params.items():
-            if key in self.VALID_PARAMETERS:
-                setattr(self, key, value)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.theta1 = ValueTracker(self.left_mass_config["theta"])
         self.theta2 = ValueTracker(self.right_mass_config["theta"])
         self.l1 = ValueTracker(self.left_mass_config["length"])
         self.l2 = ValueTracker(self.right_mass_config["length"])
+
         self.omega1 = self.left_mass_config["omega"]
         self.omega2 = self.right_mass_config["omega"]
         self.m1 = self.left_mass_config["mass"]
         self.m2 = self.right_mass_config["mass"]
         self.v = self.initial_string_velocity
-        self.create_fixed_point()
-        self.create_pulleys()
-        self.create_masses()
-        self.create_string()
 
-    def create_fixed_point(self):
-        self.fixed_point = VectorizedPoint()
-        self.add(self.fixed_point)
+        self._create_midpoint()
+        self._create_pulleys()
+        self._create_masses()
+        self._create_string()
 
-    def create_masses(self):
-        left_mass_radius = self.left_mass_config["radius"]
-        right_mass_radius = self.right_mass_config["radius"]
-        self.left_mass = Mass(radius=left_mass_radius).set_style(
-            **self.left_mass_style)
-        self.right_mass = Mass(radius=right_mass_radius).set_style(
-            **self.right_mass_style)
-        self.left_mass.get_center = self.get_left_mass_position
-        self.right_mass.get_center = self.get_right_mass_position
-        self.left_mass.add_updater(lambda m: m.move_to(m.get_center()))
-        self.right_mass.add_updater(lambda m: m.move_to(m.get_center()))
+    def _create_midpoint(self):
+        self.midpoint = VectorizedPoint()
+        self.add(self.midpoint)
+
+    def _create_masses(self):
+        r1 = self.left_mass_config["radius"]
+        r2 = self.right_mass_config["radius"]
+
+        self.left_mass = Mass(radius=r1).set_style(**self.left_mass_style)
+        self.right_mass = Mass(radius=r2).set_style(**self.right_mass_style)
+
+        self.left_mass.add_updater(lambda m: m.move_to(self.left_mass_center()))
+        self.right_mass.add_updater(lambda m: m.move_to(self.right_mass_center()))
+
+        self.left_mass.get_center = self.left_mass_center
+        self.right_mass.get_center = self.right_mass_center
+
         self.add(self.left_mass, self.right_mass)
 
-    def create_string(self):
-        self.string = AtwoodString(self)
-        self.string.set_style(**self.string_style)
+    def _create_pulleys(self):
+        self.left_pulley = Pulley(self.pulley_radius)
+        self.left_pulley.move_to(self.separation / 2 * LEFT)
+        self.left_pulley.set_style(**self.pulley_style)
+
+        self.right_pulley = Pulley(self.pulley_radius)
+        self.right_pulley.move_to(self.separation / 2 * RIGHT)
+        self.right_pulley.set_style(**self.pulley_style)
+
+        self.left_pulley.get_center = self.left_pulley_center
+        self.right_pulley.get_center = self.right_pulley_center
+
+        self.left_pulley.add_updater(left_pulley_updater(self))
+        self.right_pulley.add_updater(right_pulley_updater(self))
+
+        self.add(self.left_pulley, self.right_pulley)
+
+    def _create_string(self):
+        self.string = AtwoodString(self).set_style(**self.string_style)
         self.add(self.string)
 
-    def create_pulleys(self):
-        self.left_pulley = Pulley(radius=self.pulley_radius, num=3, ratio=0.15).move_to(
-            self.separation / 2 * LEFT).set_style(**self.pulley_style)
-        self.right_pulley = Pulley(radius=self.pulley_radius, num=3, ratio=0.15).move_to(
-            self.separation / 2 * RIGHT).set_style(**self.pulley_style)
-        self.add(self.left_pulley, self.right_pulley)
-        self.left_pulley.get_center = self.get_left_pulley_center
-        self.right_pulley.get_center = self.get_right_pulley_center
-        self.left_pulley.add_updater(left_pulley_updater(self))
-        self.right_pulley.add_updater(right_pulley_updater(self))
-
     def start(self):
-        self.left_pulley.clear_updaters()
-        self.right_pulley.clear_updaters()
-        self.set_speed(1)
+        """Start the simulation."""
+        self.add_updater(gravity_updater)
 
     def stop(self):
-        self.remove_updater(self.gravity_updater)
-        self.left_pulley.add_updater(left_pulley_updater(self))
-        self.right_pulley.add_updater(right_pulley_updater(self))
+        """Stop the simulation."""
+        self.remove_updater(gravity_updater)
+
+    def set_velocity(self, v):
+        """Set the velocity of the string. This value is used solely to
+        predict the motion of the system under the influence of gravity.
+        """
+        self.v = v
+
+    def set_omega1(self, omega):
+        """Set the left pulley angular velocity. This value is used solely
+        to predict the motion of the system under the influence of gravity.
+        """
+        self.omega1 = omega
+
+    def set_omega2(self, omega):
+        """Set the right pulley angular velocity. This value is used solely
+        to predict the motion of the system under the influence of gravity.
+        """
+        self.omega2 = omega
+
+    def left_mass_center(self):
+        u = polar_to_cartesian(self.pulley_radius, PI -
+                               self.theta1.get_value())
+        v = polar_to_cartesian(self.l1.get_value(), 3 * PI / 2 -
+                               self.theta1.get_value())
+        return self.left_pulley_center() + u + v
+
+    def right_mass_center(self):
+        u = polar_to_cartesian(self.pulley_radius,
+                               self.theta2.get_value())
+        v = polar_to_cartesian(self.l2.get_value(),
+                               self.theta2.get_value() - PI / 2)
+        return self.right_pulley_center() + u + v
+
+    def left_pulley_center(self):
+        return self.midpoint_center + LEFT * self.separation / 2
+
+    def right_pulley_center(self):
+        return self.midpoint_center + RIGHT * self.separation / 2
+
+    @property
+    def midpoint_center(self):
+        """The midpoint between the two pulleys"""
+        return self.midpoint.get_center()
 
     def step_solve(self, dt):
+        """Solves the differential equation for a dt increment in time."""
         return self.get_graph([0, dt])[1]
 
     def get_graph(self, t):
+        """Solves the differential equation related to the motion of
+        the Atwood machine.
+        """
         g, r = self.gravity, self.pulley_radius
         m1, m2 = self.m1, self.m2
         l1, l2 = self.l1.get_value(), self.l2.get_value()
@@ -132,139 +174,53 @@ class AtwoodMachine(VGroup):
 
         return odeint(atwood_diffeq_system, y0, t, args=args)
 
-    def set_string_velocity(self, v):
-        self.v = v
 
-    def set_omega1(self, omega):
-        self.omega1 = omega
-
-    def set_omega2(self, omega):
-        self.omega2 = omega
-
-    def get_left_mass_position(self):
-        u = polar_to_cartesian(self.pulley_radius, PI -
-                               self.theta1.get_value())
-        v = polar_to_cartesian(self.l1.get_value(), 3 *
-                               PI / 2 - self.theta1.get_value())
-        return self.get_left_pulley_center() + u + v
-
-    def get_right_mass_position(self):
-        u = polar_to_cartesian(self.pulley_radius, self.theta2.get_value())
-        v = polar_to_cartesian(self.l2.get_value(),
-                               self.theta2.get_value() - PI / 2)
-        return self.get_right_pulley_center() + u + v
-
-    def get_left_pulley_center(self):
-        return self.fixed_center + LEFT * self.separation / 2
-
-    def get_right_pulley_center(self):
-        return self.fixed_center + RIGHT * self.separation / 2
-
-    @property
-    def fixed_center(self):
-        return self.fixed_point.get_center()
-
-    def set_speed(self, ratio):
-        def updater(m, dt):
-            return gravity_updater(m, ratio * dt)
-        self.add_updater(updater)
-        self.gravity_updater = updater
-
-
-class AtwoodMachineWithMeasurements(AtwoodMachine):
-
-    def __init__(self, **params):
-        super().__init__(**params)
-        self.create_braces()
-        self.create_angles()
-
-    def create_braces(self, buff=MED_SMALL_BUFF):
-        self.left_brace = always_redraw(lambda: stretch(
-            Brace(self.left_line, self.left_brace_dir, buff),
-            0.5, self.left_brace_dir
-        ))
-        self.right_brace = always_redraw(lambda: stretch(
-            Brace(self.right_line, direction=self.right_brace_dir),
-            0.5, self.right_brace_dir
-        ))
-
-    def get_left_brace_tex(self, *tex, buff=MED_LARGE_BUFF, scale=0.5):
-        tex = f_always(
-            self.left_brace.get_tex(*tex).scale(scale).move_to,
-            lambda: self.left_line.get_center() + self.left_brace_dir * buff
-        )
-        return tex
-
-    def get_right_brace_tex(self, *tex, buff=MED_LARGE_BUFF, scale=0.5):
-        tex = f_always(
-            self.right_brace.get_tex(*tex).scale(scale).move_to,
-            lambda: self.right_line.get_center() + self.right_brace_dir * buff
-        )
-        return tex
-
-    @property
-    def left_line(self):
-        return self.string.left_line
-
-    @property
-    def right_line(self):
-        return self.string.right_line
-
-    @property
-    def left_line_dir(self):
-        return self.string.left_line.get_unit_vector()
-
-    @property
-    def right_line_dir(self):
-        return self.string.right_line.get_unit_vector()
-
-    @property
-    def left_brace_dir(self):
-        return self.left_line.copy().rotate(-PI / 2).get_unit_vector()
-
-    @property
-    def right_brace_dir(self):
-        return self.right_line.copy().rotate(PI / 2).get_unit_vector()
-
-
-def gravity_updater(m, dt):
+def gravity_updater(m: AtwoodMachine, dt):
+    """Updater function that describes the motion of the system."""
     x, v, theta1, omega1, theta2, omega2 = m.step_solve(dt)
     l1 = m.l1.get_value() - x + m.pulley_radius * (theta1 - m.theta1.get_value())
     l2 = m.l2.get_value() + x + m.pulley_radius * (theta2 - m.theta2.get_value())
 
-    m.set_string_velocity(v)
-    m.theta1.set_value(theta1)
-    m.set_omega1(omega1)
-    m.theta2.set_value(theta2)
-    m.set_omega2(omega2)
-
+    m.set_velocity(v)
     m.l1.set_value(l1)
     m.l2.set_value(l2)
-    m.left_pulley.rotate(-x / m.pulley_radius)
-    m.right_pulley.rotate(-x / m.pulley_radius)
+    m.set_omega1(omega1)
+    m.set_omega2(omega2)
+    m.theta2.set_value(theta2)
+    m.theta1.set_value(theta1)
 
 
-def left_pulley_updater(atwood):
-    atwood.last_l1 = atwood.l1.get_value()
-    atwood.last_theta1 = atwood.theta1.get_value()
+def left_pulley_updater(atwood: AtwoodMachine):
+    """Updater function for the left pulley."""
+    old_l = atwood.l1.get_value()
+    old_theta = atwood.theta1.get_value()
 
-    def updater(m):
-        x = atwood.l1.get_value() - atwood.last_l1 - atwood.pulley_radius * (
-            atwood.theta1.get_value() - atwood.last_theta1)
-        m.rotate(x / atwood.pulley_radius)
-        atwood.last_l1 = atwood.l1.get_value()
-        atwood.last_theta1 = atwood.theta1.get_value()
+    def updater(pulley: Mobject):
+        nonlocal old_l, old_theta
+
+        dl = atwood.l1.get_value() - old_l
+        dtheta = atwood.theta1.get_value() - old_theta
+        angle = dl / atwood.pulley_radius - dtheta
+
+        old_l = atwood.l1.get_value()
+        old_theta = atwood.theta1.get_value()
+        pulley.rotate(angle)
     return updater
 
 
-def right_pulley_updater(atwood):
-    atwood.last_l2 = atwood.l2.get_value()
-    atwood.last_theta2 = atwood.theta2.get_value()
+def right_pulley_updater(atwood: AtwoodMachine):
+    """Updater function for the right pulley."""
+    old_l = atwood.l2.get_value()
+    old_theta = atwood.theta2.get_value()
 
-    def updater(m):
-        x = - atwood.l2.get_value() + atwood.last_l2 + atwood.pulley_radius * (
-            atwood.theta2.get_value() - atwood.last_theta2)
-        m.rotate(x / atwood.pulley_radius)
-        atwood.last_l2 = atwood.l2.get_value()
-        atwood.last_theta2 = atwood.theta2.get_value()
+    def updater(pulley: Mobject):
+        nonlocal old_l, old_theta
+
+        dl = atwood.l2.get_value() - old_l
+        dtheta = atwood.theta2.get_value() - old_theta
+        angle = dtheta - dl / atwood.pulley_radius
+
+        old_l = atwood.l2.get_value()
+        old_theta = atwood.theta2.get_value()
+        pulley.rotate(angle)
     return updater
--- diffeq.py
+++ diffeq.py
@@ -1,10 +1,46 @@
+from typing import List
 from numpy import sin, cos
 
 
-def atwood_diffeq_system(y, t, g, r, m1, m2, l1, l2):
+def atwood_diffeq_system(
+    y: List[float],
+    t: float,
+    g: float = 9.86,
+    r: float = 0.1,
+    m1: float = 1,
+    m2: float = 1,
+    l1: float = 1,
+    l2: float = 1,
+) -> List[float]:
+    """The set of equations that describe the motion of the system.
+
+    :param y: A list of variables, which are:
+              - `x`: a value measures the position of the string
+              - `v`: the derivative of x with respect to time
+              - `theta1`: the angle between the left string and the vertical
+              - `omega1`: the derivate of `theta1` with respect to time
+              - `theta2`: the angle between the right string and the vertical
+              - `omega2`: theta derivative of `theta2` with respect to time
+    :param t: The time in seconds. This value does not really matter, but must
+              be listed here for the sake of the differential equation solving
+              algorithm employed by numpy.
+    :param g: The value of gravity in meters per second squared.
+    :param r: The radius of the pulleys in meters.
+    :param m1: The mass of the left mass in kilograms.
+    :param m2: The mass of the right mass in kilograms.
+    :param l1: The length of the left side string, from where it touches the
+               pulley to the center of the left mass.
+    :param l2: The length of the right side string, from where it touches the
+               pulley to the center of the right mass.
+    :returns: The derivative of `y` with respect to time. This function does
+              not calculate the derivative of `y` in a general case; just when
+              the motion of the system matches that of an Atwood Machine
+    """
     x, v, theta1, omega1, theta2, omega2 = y
-    alfa1 = (omega1**2 * r - g * sin(theta1) - 2 * (r * omega1 - v) * omega1) / l1
-    alfa2 = (omega2**2 * r - g * sin(theta2) - 2 * (v + r * omega2) * omega2) / l2
+    alfa1 = (omega1**2 * r - g * sin(theta1) -
+             2 * (r * omega1 - v) * omega1) / l1
+    alfa2 = (omega2**2 * r - g * sin(theta2) -
+             2 * (v + r * omega2) * omega2) / l2
     a = (m2 * (l2 * omega2**2 + g * cos(theta2)) -
          m1 * (l1 * omega1**2 + g * cos(theta1))) / (m1 + m2)
     return [v, a, omega1, alfa1, omega2, alfa2]
